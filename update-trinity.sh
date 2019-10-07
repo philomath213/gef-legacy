5c8235279ba0fdf9bb7c03ac7ee20bf6191905cb
philomath213 <bilalphilomath@gmail.com>
2019-10-07 21:13:13 +0100

    remove sudo from update-trinity.sh

--- update-trinity.sh
+++ update-trinity.sh
@@ -2,7 +2,7 @@
 #
 # Install/update Capstone/Keystone/Unicorn from GitHub with Python/Python3 bindings
 # First time, run:
-# $ sudo apt-get install git cmake gcc g++ pkg-config libglib2.0-dev
+# # apt-get install git cmake gcc g++ pkg-config libglib2.0-dev
 #
 
 LOGFILE=/tmp/update-trinity.log
@@ -22,9 +22,9 @@ git clone --quiet https://github.com/keystone-engine/keystone.git
 mkdir -p keystone/build && cd keystone/build
 sed -i "s/make -j8/make -j${NB_CPU}/g" ../make-share.sh
 ../make-share.sh
-sudo make install
+make install
 cd ../bindings/python
-sudo make install install3
+make install install3
 popd
 echo "[+] Done" >&2
 
@@ -33,9 +33,9 @@ pushd /tmp
 git clone --quiet https://github.com/aquynh/capstone.git
 cd capstone
 ./make.sh default -j${NB_CPU}
-sudo ./make.sh install
+./make.sh install
 cd ./bindings/python
-sudo make install install3
+make install install3
 popd
 echo "[+] Done" >&2
 
@@ -44,14 +44,14 @@ pushd /tmp
 git clone --quiet https://github.com/unicorn-engine/unicorn.git
 cd unicorn
 UNICORN_QEMU_FLAGS="--python=`which python2`" MAKE_JOBS=${NB_CPU} ./make.sh
-sudo ./make.sh install
+./make.sh install
 cd ./bindings/python
-sudo make install install3
+make install install3
 popd
 echo "[+] Done" >&2
 
 echo "[+] Cleanup" >&2
-sudo rm -fr -- /tmp/{keystone,capstone,unicorn}
-sudo ldconfig
+rm -fr -- /tmp/{keystone,capstone,unicorn}
+ldconfig
 
 popd
